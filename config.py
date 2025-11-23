0d2862df60e69452e5b5fd5c26b8bbb018a30fb1
Charles Harrison <35784256+notxcharles@users.noreply.github.com>
2025-11-23 23:06:08 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# Individuals
+# One of the stopping criteria is a chromosome of [1] * CHROMOSOME_LENGTH
+CHROMOSOME_LENGTH = 10
+# When a new individual is randomly created, what is the chance that one of their genes will be a 1
+CHROMOSOME_GENERATION_1_CHANCE = 0.1
+
+
+CROSSOVER_POINT = 0
+# Percentage
+MUTATION_RATE = 0.01
