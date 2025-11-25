f6df8b92a9b8b1805b6f3a095ddcbc6be94cabaa
Charles Harrison <35784256+notxcharles@users.noreply.github.com>
2025-11-25 15:17:53 +0000

    Update config.py
    Added crossover point and mutation chance

--- config.py
+++ config.py
@@ -1,16 +1,21 @@
 # Individuals
 # One of the stopping criteria is a chromosome of [1] * CHROMOSOME_LENGTH
 CHROMOSOME_LENGTH = 10
+
 # When a new individual is randomly created, what is the chance that one of their genes will be a 1
 CHROMOSOME_GENERATION_1_CHANCE = 0.1
 
 
 POPULATION_SIZE = 100
+
 MAX_GENERATIONS = 100
+
+# Using elites selection, how many of the fittest individual qualify as elites?
 ELITES_SIZE = 2
-# percentage of the population for the tournament
+# percentage of the population for the tournament, 0.2=20%
 TOURNAMENT_SELECTION_PERCENT = 0.2
 
-CROSSOVER_POINT = 0
-# Percentage
-MUTATION_RATE = 0.01
+
+CROSSOVER_POINT = 5
+# Percentage, 0.01 = 1%
+MUTATION_CHANCE = 0.01
